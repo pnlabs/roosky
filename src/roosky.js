86d7de5a1d4f556093cf3c0925fc06bdbf0910df
Pavel Mudrov <p.mudrov@vanguardsoft.ru>
2020-10-15 17:47:48 +0300

    removed logs

--- src/roosky.js
+++ src/roosky.js
@@ -1,76 +1,71 @@
-const c = 'бвгджзклмнпрстфхцчшщ'
-const v = 'аеиоуэюя'
-const lnTails = ['ов', 'ин', 'ев']
-const posTails = ['ер', 'ор', 'ист', 'ик']
+"use strict"
+
+const c = "бвгджзклмнпрстфхцчшщ"
+const v = "аеиоуэюя"
+const lnTails = ["ов", "ин", "ев"]
+const posTails = ["ер", "ор", "ист", "ик"]
 
 const randomChar = (s) => {
-    const position = Math.trunc(Math.random() * s.length)
-    return s.slice(position, position + 1)
+	const position = Math.trunc(Math.random() * s.length)
+	return s.slice(position, position + 1)
 }
 
 const randomTail = (tails) => {
-    const position = Math.trunc(Math.random() * tails.length)
-    return tails[position]
+	const position = Math.trunc(Math.random() * tails.length)
+	return tails[position]
 }
 
 const firstName = () => {
-    const sylls = Math.trunc(Math.random() * 3 + 1)
-    console.log('firstName sylls', sylls)
-    let name = ''
-    for (let s = 0; s < sylls; s++) {
-        console.log(`firstName s`,s)
-        name =
-            name +
-            (s === 0 ? randomChar(c).toUpperCase() : randomChar(c)) +
-            randomChar(v)
-        console.log(`firstName name`,name)
-    }
-    /*
+	const sylls = Math.trunc(Math.random() * 3 + 1)
+	let name = ""
+	for (let s = 0; s < sylls; s++) {
+		name =
+			name +
+			(s === 0 ? randomChar(c).toUpperCase() : randomChar(c)) +
+			randomChar(v)
+	}
+	/*
         if (Math.random() > 0.5 && gender > 0) {
             name = name + randomChar(c)
         }
     */
-    name = name + randomChar(c)
-    return name
+	name = name + randomChar(c)
+	return name
 }
 
 const lastName = () => {
-    const sylls = Math.trunc(Math.random() * 4 + 1)
-    console.log(`lastName sylls`, sylls)
-    let name = ''
-    for (let s = 0; s < sylls; s++) {
-        console.log(`lastName s`,s)
-        name =
-            name +
-            (s === 0 ? randomChar(c).toUpperCase() : randomChar(c)) +
-            randomChar(v)
-        console.log(`lastName name`,name)
-    }
-    name = name + randomChar(c)
-    name = name + randomTail(lnTails)
-    return name
+	const sylls = Math.trunc(Math.random() * 4 + 1)
+	let name = ""
+	for (let s = 0; s < sylls; s++) {
+		name =
+			name +
+			(s === 0 ? randomChar(c).toUpperCase() : randomChar(c)) +
+			randomChar(v)
+	}
+	name = name + randomChar(c)
+	name = name + randomTail(lnTails)
+	return name
 }
 
 const middleName = () => {
-    return lastName() + 'ич'
+	return lastName() + "ич"
 }
 
 const phone = () => {
-    return (
-        '+7' 
-        + String(Math.trunc(Math.random() * 19) + 881)
-        + String(Math.trunc(Math.random() * 10000000)).padStart(7, '0')
-    )
+	return (
+		"+7" +
+		String(Math.trunc(Math.random() * 19) + 881) +
+		String(Math.trunc(Math.random() * 10000000)).padStart(7, "0")
+	)
 }
 
 const position = () => {
-    const sylls = Math.trunc(Math.random() * 4 + 1)
-    let string = ''
-    for (let s = 0; s < sylls; s++) {
-        string = string + randomChar(c) + randomChar(v)
-    }
-    return string + randomTail(posTails)
+	const sylls = Math.trunc(Math.random() * 4 + 1)
+	let string = ""
+	for (let s = 0; s < sylls; s++) {
+		string = string + randomChar(c) + randomChar(v)
+	}
+	return string + randomTail(posTails)
 }
 
 export default { firstName, lastName, middleName, phone, position }
-
