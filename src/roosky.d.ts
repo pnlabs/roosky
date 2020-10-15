cc80edf8d5ee79e19e244fc83cb0f372da7fa7b0
Pavel Mudrov <p.mudrov@vanguardsoft.ru>
2020-10-15 19:16:45 +0300

    added types

---
+++ src/roosky.d.ts
@@ -0,0 +1,5 @@
+declare function firstName(): string
+declare function lastName(): string
+declare function middleName(): string
+declare function phone(): string
+declare function position(): string
